e00b7b680698e1d38b6072d1f189f5b59d4aad1c
sudaning <sudaning@sina.com>
2017-02-21 14:45:02 +0800

    增加python-esl的依赖库swig

--- setup.py
+++ setup.py
@@ -6,22 +6,6 @@ author_email = 'sudaning@sina.com'
 description = "A pure Python library designed to make it easy and quick to code for Neko"
 
 long_description = '''
-=====
-Neko
-=====
-Introduction
-------------
-pyNeko is a pure Python library designed to making magic to code for Neko.
-You can use pyNeko to making magic beautiful.
-In `/scripts <https://github.com/sudaning/PytLab-Neko/tree/master/scripts>`_ , there are some scripts written by me for daily use.
-Installation
-------------
-1. Via pip(recommend)::
-	pip install pyNeko
-2. Via easy_install::
-	easy_install pyNeko
-3. From source::
-	python setup.py install
 '''
 
 install_requires = [
